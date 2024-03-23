885d8e9c76f1cbe07b2445d9a29208e95fb2878c
Wyatt Ades <me@wyattades.com>
2024-03-23 12:37:33 -0400

    disable default screenshots

--- lib/cyperful/framework_injections.rb
+++ lib/cyperful/framework_injections.rb
@@ -51,6 +51,12 @@ module Cyperful::SystemTestHelper
     Cyperful.teardown(error)
     super
   end
+
+  # Disable default screenshot on failure b/c we handle them ourselves.
+  # https://github.com/rails/rails/blob/main/actionpack/lib/action_dispatch/system_testing/test_helpers/screenshot_helper.rb#L156
+  def take_failed_screenshot
+    nil
+  end
 end
 
 # we need to allow the iframe to be embedded in the cyperful server
